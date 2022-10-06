94e0c3cfb94304b3afa69035942383affa5bf883
sovisrushain <sovisrushain@gmail.com>
2022-10-06 15:48:16 +0530

    initial commit

---
+++ src/test/java/com/cisco/dataredis/DataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.dataredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
