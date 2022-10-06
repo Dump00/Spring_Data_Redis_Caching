a3ce5ddca8d8235a38b3b86d34c0db1d900f8a53
sovisrushain <sovisrushain@gmail.com>
2022-10-06 16:11:03 +0530

    redis configuration

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Spring Data Redis
 
 <br>
+
+## The Redis Configuration
+To define the connection settings between the application client and the Redis server instance, we need to use a Redis client.
+Here we'll use **Jedis — a simple and powerful Redis client implementation**.
---
+++ src/main/java/com/cisco/dataredis/model/Product.java
@@ -0,0 +1,20 @@
+package com.cisco.dataredis.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash("Product")
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private int qty;
+    private long price;
+}
